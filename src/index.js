43086b37cfc4161fa787e11f3b83c43bbb30d162
bokuweb <bokuweb12@gmail.com>
2016-04-10 07:37:28 +0900

    Fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const Nightmare = require('nightmare');
 const vo = require('vo');
+const util = require('util');
 
 const browserWidth = 1200;
 const browserHeight = 800;
@@ -18,9 +19,27 @@ function *run() {
             return body.scrollHeight;
           });
 
-  console.dir(height);
+  const nodes =  yield nightmare.goto('http://blackrockdigital.github.io/startbootstrap-grayscale/')
+          .wait('body')
+          .evaluate(function() {
+            // jQuery obj?
+            //return document.body.style.display
+            var l = [];
+            const body = document.querySelector('body');
+            // var nodes =  body.childNodes;
+            for(var i = 0; i < body.childNodes.length; i ++) {
+              if (body.childNodes[i].style) l.push(body.childNodes[i].style.display)
+            }
+            return l;
+          })
+
+  console.log(util.inspect(nodes));
+
+  console.log(height);
   for (var i = 0; i < height / browserHeight; i ++) {
     yield nightmare.viewport(browserWidth, browserHeight)
+      .wait('body')
+      // のこりheightがブラウザ高さ未満の場合veiwポートを変更？して高さをあわせる？
       .scrollTo(browserHeight * i, 0)
       .wait(100)
       .screenshot(require('path').join('./', `grayscale${i}.png`));
@@ -31,3 +50,23 @@ function *run() {
 vo(run)(function() {
   console.log('done');
 });
+
+function searchNodes(root) {
+  var list = [];
+  var search = function (node)
+  {
+    while (node != null)
+    {
+      // 自分を処理
+      list.push(node);
+      // 子供を再帰
+      search(node.first());
+      // 次のノードを探査
+      node = node.next();
+    }
+  }
+  search(root.first());
+  return list;
+}
+
+ 
